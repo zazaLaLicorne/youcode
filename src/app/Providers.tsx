30f18b3fc885e609cf3fcc9d42c0d3696c25f8fd
ilamarre <isabellelamarre62@gmail.com>
2025-06-12 15:12:56 +0200

    first commit

---
+++ src/app/Providers.tsx
@@ -0,0 +1,18 @@
+// app/Providers.
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from 'next-themes';
+import { PropsWithChildren } from 'react';
+import { Toaster } from 'react-hot-toast';
+
+export const Providers = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Toaster />
+        <SessionProvider>{children}</SessionProvider>
+      </ThemeProvider>
+    </>
+  );
+};
