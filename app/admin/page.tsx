cebe63b988944b5331012c292d1ca4f93cba0798
ilamarre <isabellelamarre62@gmail.com>
2025-06-14 18:50:35 +0200

    task4

---
+++ app/admin/page.tsx
@@ -0,0 +1,20 @@
+import {
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from '@/components/layout/layout';
+import Link from 'next/link';
+
+export default async function CoursesPage() {
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Courses</LayoutTitle>
+      </LayoutHeader>
+      <LayoutContent>
+        <Link href="/admin/courses">Courses</Link>
+      </LayoutContent>
+    </Layout>
+  );
+}
